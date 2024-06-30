606df6ff132fb7988289ba5caebc9c98f2ae0755
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-06-30 10:45:32 -0300

    Feat: adicionando controller base Adicionando controller base ja com post de exemplo com model de entrada e model de saida(response)

---
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Request;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // criação de usuario(tipo post)
+        [HttpPost]
+        // vai retornar de resposta o ResponseRegisterUserJson com status 201 created
+        [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace MyRecipeBook.Communication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
